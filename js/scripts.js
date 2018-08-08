7d54e1866958adee1bda8ff1c1eeff584dd0462c
Brian Nelson and Connor McCarthy <connor@>
2018-08-08 15:49:45 -0700

    add js to display contact info, show all contact info when name clicked on.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,20 @@
 $(document).ready(function(){
-  $("form#survey").submit(function(event){
+  $("form#contact").submit(function(event){
     event.preventDefault();
-
+    var fNameInput= $("#fName").val();
+    var lNameInput = $("#lName").val();
+    var addressInput = $("#address").val();
+    var emailInput = $("#email").val();
+    var phoneNumberInput = $("#phone-number").val();
+    var additionalDetailsInput = $("#additional-details").val();
+    $("#output #fName").text(fNameInput);
+    $("#output #lName").text(lNameInput);
+    $("#output #address").text(addressInput);
+    $("#output #email").text(emailInput);
+    $("#output #phone-number").text(phoneNumberInput);
+    $("#output #additional-details").text(additionalDetailsInput);
+    $(".clickable").click(function(){
+      $(".info-hidden").toggle();
+    });
   });
 });
